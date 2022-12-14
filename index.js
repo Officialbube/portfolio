c6ea43c729fb78460b021d1ff4b5c8664a569b4b
Officialbube <ebubenkereebube@gmail.com>
2022-12-14 21:16:45 +0100

    added menu

---
+++ index.js
@@ -0,0 +1,8 @@
+function clicked(){
+    document.getElementById('hamburger').classList.toggle('change');
+    document.getElementById('menu').classList.toggle('change');
+    document.getElementById('logo').classList.toggle('logo-change');
+    document.getElementById('header').classList.toggle('nav-header');
+    document.getElementById('menuText').classList.toggle('close');
+    document.getElementById('close').classList.toggle('show');
+}
